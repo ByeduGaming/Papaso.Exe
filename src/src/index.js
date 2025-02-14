502510c53f70e912c711dd34d74bb03c9dab84bf
ByeduGaming <eduardovalencia2016@gmail.com>
2025-02-13 23:47:55 -0500

    Agregando la carta interactiva

---
+++ src/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Cartainteractiva from "./Cartainteractiva"; // Importa el componente
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Cartainteractiva />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
